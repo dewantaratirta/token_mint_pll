55d1f01e4b943ae749dd53d8b7f5563fb23d3fdc
dewantara tirta <dewantara.tirta@sirclo.com>
2024-05-05 02:12:33 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,57 @@
+import dotenv from "dotenv";
+import { ethers } from "ethers";
+import * as fs from "node:fs";
+
+const env = dotenv.config()?.parsed;
+
+const alchemyProvider = new ethers.AlchemyProvider(
+  "sepolia",
+  env.ALCHEMY_API_KEY
+);
+
+// load wallet from mnemonic
+const wallet = ethers.Wallet.fromPhrase(process.env?.MNEMONIC || env?.MNEMONIC);
+const { address: wallet_address, privateKey } = wallet;
+
+console.log(`Your wallet address: ${wallet_address}`);
+
+// read the abi file from contract
+const abi = fs.readFileSync("contract_abi.txt", "utf8");
+
+const mint = async () => {
+  console.log("Minting Token");
+  const signer = new ethers.Wallet(privateKey, alchemyProvider);
+  const parallel_contract = new ethers.Contract(
+    env.CONTRACT_ADDRESS,
+    abi,
+    wallet_address
+  );
+  const contractWithSigner = await parallel_contract.connect(signer);
+
+  try {
+    const tx = await contractWithSigner.mint(wallet_address);
+    await tx.wait();
+    console.log("Transaction confirmed! Token successfully claimed!");
+  } catch (err) {
+    console.log("Error:" + err.message);
+  }
+};
+
+// random minting timeout to avoid suspicion
+const min_timeout = 1000;
+const max_timeout = 2000;
+
+const rand = () => {
+  let res = Math.random() * (max_timeout - min_timeout) + min_timeout;
+  console.log(`random time: ${res}`);
+  return res;
+};
+
+const start = async () => {
+  setTimeout(async () => {
+    await mint();
+    start();
+  }, rand());
+};
+
+start();
---
+++ readme.md
@@ -0,0 +1,24 @@
+
+
+### Install dependency
+
+```
+yarn install
+```
+
+
+### Copy env
+```
+cp .env-example .env
+```
+
+
+### Run Program
+```
+yarn start
+```
+add mnemonic to run your other account
+
+```
+MNEMONIC="your mnemonic" yarn start
+```
